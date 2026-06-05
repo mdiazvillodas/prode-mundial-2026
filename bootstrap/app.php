89a93cd371e1768b082093d14ab27f41aa53b28e
Mariano <mariano.diazvillodas@gmail.com>
2026-06-05 11:02:31 +0200

    Fix HTTPS URL generation on Railway

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,7 @@ use Illuminate\Foundation\Application;
 use App\Http\Middleware\EnsureUserIsAdmin;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Symfony\Component\HttpFoundation\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -12,6 +13,17 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        if (in_array(env('APP_ENV'), ['production', 'staging'], true) || env('RAILWAY_ENVIRONMENT') || env('RAILWAY_PROJECT_ID')) {
+            $middleware->trustProxies(
+                at: '*',
+                headers: Request::HEADER_X_FORWARDED_FOR
+                    | Request::HEADER_X_FORWARDED_HOST
+                    | Request::HEADER_X_FORWARDED_PORT
+                    | Request::HEADER_X_FORWARDED_PROTO
+                    | Request::HEADER_X_FORWARDED_PREFIX
+            );
+        }
+
         $middleware->alias([
             'admin' => EnsureUserIsAdmin::class,
         ]);
